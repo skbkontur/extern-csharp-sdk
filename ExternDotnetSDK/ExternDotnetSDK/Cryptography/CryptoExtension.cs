9f7df9b95c14002bde7324bc75bc354a214a07f4
Andrew <fakmaki99@mail.ru>
2020-11-30 18:04:12 +0500

    Added Extension to get Basee64 of X509Certificate2

--- ExternDotnetSDK/ExternDotnetSDK/Cryptography/CryptoExtension.cs
+++ ExternDotnetSDK/ExternDotnetSDK/Cryptography/CryptoExtension.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Kontur.Extern.Client.Cryptography
 {
     public static class BytesExtension
     {
         /// <summary>
-        /// Сравнение двух массивов байт
+        ///     Сравнение двух массивов байт
         /// </summary>
         /// <param name="d1"></param>
         /// <param name="d2"></param>
@@ -17,14 +16,25 @@ namespace Kontur.Extern.Client.Cryptography
             if (d1 == d2) return 0;
             if (d1 == null) return -1;
             if (d2 == null) return 1;
-            int data1Length = d1.Length;
-            int data2Length = d2.Length;
-            for (int i = 0; i < Math.Min(data1Length, data2Length); ++i)
+            var data1Length = d1.Length;
+            var data2Length = d2.Length;
+            for (var i = 0; i < Math.Min(data1Length, data2Length); ++i)
             {
-                int comparison = d1[i].CompareTo(d2[i]);
+                var comparison = d1[i].CompareTo(d2[i]);
                 if (comparison != 0) return comparison;
             }
+
             return data1Length.CompareTo(data2Length);
+        }
+
+        /// <summary>
+        /// Получение сертификата в кодировке Base64
+        /// </summary>
+        /// <param name="cert">сертификат</param>
+        /// <returns>Сертификат в base64</returns>
+        public static string GetBase64(this X509Certificate2 cert)
+        {
+            return Convert.ToBase64String(cert.Export(X509ContentType.Cert));
         }
     }
 }
