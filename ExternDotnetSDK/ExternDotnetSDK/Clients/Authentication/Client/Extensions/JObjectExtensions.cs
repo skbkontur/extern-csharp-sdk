e94285d02e1f332df0f62c83729b6df1b9ad02d5
Andrew <fakmaki99@mail.ru>
2021-03-03 19:26:30 +0500

    Base extensions for auth providers

---
+++ ExternDotnetSDK/ExternDotnetSDK/Clients/Authentication/Client/Extensions/JObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Newtonsoft.Json.Linq;
+
+namespace Kontur.Extern.Client.Clients.Authentication.Client.Extensions
+{
+    internal static class JObjectExtensions
+    {
+        public static IEnumerable<Claim> ToClaims(this JObject json)
+        {
+            foreach (var x in json)
+            {
+                if (x.Value is JArray array)
+                {
+                    foreach (var item in array)
+                    {
+                        yield return new Claim(x.Key, item.ToString());
+                    }
+                }
+                else
+                {
+                    yield return new Claim(x.Key, x.Value.ToString());
+                }
+            }
+        }
+    }
+}
