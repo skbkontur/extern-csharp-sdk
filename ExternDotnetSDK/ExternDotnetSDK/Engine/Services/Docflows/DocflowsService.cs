f078fc75fda7ce6e9cdb5e9a6c077d99c0bfdc9b
Andrew <fakmaki99@mail.ru>
2021-06-23 10:14:27 +0500

    Docflows service without accountId and timeout

---
+++ ExternDotnetSDK/ExternDotnetSDK/Engine/Services/Docflows/DocflowsService.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kontur.Extern.Client.Clients.Docflows;
+using Kontur.Extern.Client.Models.Api;
+using Kontur.Extern.Client.Models.Common;
+using Kontur.Extern.Client.Models.Docflows;
+using Kontur.Extern.Client.Models.Documents;
+
+namespace Kontur.Extern.Client.Engine.Services.Docflows
+{
+    //todo Сделать нормальные тесты для методов.
+    public class DocflowsService : IDocflowsService
+    {
+        public IDocflowsClient Client { get; }
+        private readonly IAccountIdProvider accountIdProvider;
+        private readonly TimeSpan? timeout;
+
+        public DocflowsService(IAccountIdProvider accountIdProvider, IDocflowsClient docflowsClient, TimeSpan? timeout)
+        {
+            this.accountIdProvider = accountIdProvider;
+            Client = docflowsClient;
+            this.timeout = timeout;
+        }
+
+        public Task<DocflowPage> GetDocflowsAsync(DocflowFilter filter = null) => Client.GetDocflowsAsync(accountIdProvider.AccountId, filter, timeout);
+
+        public Task<DocflowPage> GetRelatedDocflows(
+            Guid relatedDocflowId,
+            Guid relatedDocumentId,
+            DocflowFilter filter = null)
+            => Client.GetRelatedDocflows(accountIdProvider.AccountId, relatedDocflowId, relatedDocumentId, filter, timeout);
+
+        public Task<Docflow> GetDocflowAsync(Guid docflowId)
+            => Client.GetDocflowAsync(accountIdProvider.AccountId, docflowId, timeout);
+
+        public Task<List<Document>> GetDocumentsAsync(Guid docflowId)
+            => Client.GetDocumentsAsync(accountIdProvider.AccountId, docflowId, timeout);
+
+        public Task<Document> GetDocumentAsync(Guid docflowId, Guid documentId)
+            => Client.GetDocumentAsync(accountIdProvider.AccountId, docflowId, documentId, timeout);
+
+        public Task<DocflowDocumentDescription> GetDocumentDescriptionAsync(Guid docflowId, Guid documentId)
+            => Client.GetDocumentDescriptionAsync(accountIdProvider.AccountId, docflowId, documentId, timeout);
+
+        public Task<List<Signature>> GetDocumentSignaturesAsync(Guid docflowId, Guid documentId)
+            => Client.GetDocumentSignaturesAsync(accountIdProvider.AccountId, docflowId, documentId, timeout);
+
+        public Task<Signature> GetSignatureAsync(Guid docflowId, Guid documentId, Guid signatureId)
+            => Client.GetSignatureAsync(
+                accountIdProvider.AccountId,
+                docflowId,
+                documentId,
+                signatureId,
+                timeout);
+
+        public Task<byte[]> GetSignatureContentAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid signatureId)
+            => Client.GetSignatureContentAsync(accountIdProvider.AccountId, docflowId, documentId, signatureId, timeout);
+
+        public Task<PrintDocumentResult> PrintDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid contentId)
+            => Client.PrintDocumentAsync(accountIdProvider.AccountId, docflowId, documentId, contentId, timeout);
+
+        public Task<ApiTaskResult<PrintDocumentResult>> StartPrintDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid contentId)
+            => Client.StartPrintDocumentAsync(accountIdProvider.AccountId, docflowId, documentId, contentId, timeout);
+
+        public Task<ApiTaskResult<PrintDocumentResult>> GetPrintTaskAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid taskId)
+            => Client.StartPrintDocumentAsync(accountIdProvider.AccountId, docflowId, documentId, taskId, timeout);
+
+        public Task<CloudDecryptionInitResult> StartCloudDecryptDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            byte[] certificate)
+            => Client.StartCloudDecryptDocumentAsync(accountIdProvider.AccountId, docflowId, documentId, certificate, timeout);
+
+        public Task<DecryptDocumentResult> ConfirmDocumentDecryptionAsync(
+            Guid docflowId,
+            Guid documentId,
+            string requestId,
+            string code,
+            bool? unzip = null)
+            => Client.ConfirmDocumentDecryptionAsync(
+                accountIdProvider.AccountId,
+                docflowId,
+                documentId,
+                requestId,
+                code,
+                unzip,
+                timeout);
+
+        public Task<DssDecryptionInitResult> StartDssDecryptDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            byte[] certificate,
+            bool? unzip = null)
+            => Client.StartDssDecryptDocumentAsync(
+            accountIdProvider.AccountId,
+            docflowId,
+            documentId,
+            certificate,
+            unzip,
+            timeout);
+
+        public Task<ApiTaskResult<DecryptDocumentResult>> GetDssDecryptDocumentTaskAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid taskId)
+                => Client.GetDssDecryptDocumentTaskAsync(
+            accountIdProvider.AccountId,
+            docflowId,
+            documentId,
+            taskId,
+            timeout);
+
+        public Task<RecognizeResult> RecognizeDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid contentId  )
+            => Client.RecognizeDocumentAsync(
+            accountIdProvider.AccountId,
+            docflowId,
+            documentId,
+            contentId,
+            timeout);
+    }
+}
---
+++ ExternDotnetSDK/ExternDotnetSDK/Engine/Services/Docflows/IDocflowsService.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kontur.Extern.Client.Clients.Docflows;
+using Kontur.Extern.Client.Models.Api;
+using Kontur.Extern.Client.Models.Common;
+using Kontur.Extern.Client.Models.Docflows;
+using Kontur.Extern.Client.Models.Documents;
+
+namespace Kontur.Extern.Client.Engine.Services.Docflows
+{
+    public interface IDocflowsService
+    {
+        IDocflowsClient Client { get; }
+
+        /// <summary>
+        /// Поиск документооборотов по заданным параметрам
+        /// </summary>
+        /// <param name="filter">Параметры поиска</param>
+        /// <returns>Список документооборотов</returns>
+        Task<DocflowPage> GetDocflowsAsync(DocflowFilter filter = null);
+
+        /// <summary>
+        /// Получение связанных документооборотов
+        /// </summary>
+        /// <param name="relatedDocflowId">Документооборот, к которому относится связанный документооборот</param>
+        /// <param name="relatedDocumentId">Документ, к которому относится связанный документооборот</param>
+        /// <param name="filter">Параметры поиска</param>
+        /// <returns>Список документооборотов</returns>
+        Task<DocflowPage> GetRelatedDocflows(
+            Guid relatedDocflowId,
+            Guid relatedDocumentId,
+            DocflowFilter filter = null);
+
+        /// <summary>
+        /// Получение документооборота по идентификатору
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <returns>Документооборот</returns>
+        Task<Docflow> GetDocflowAsync(Guid docflowId);
+
+        /// <summary>
+        /// Получение всех документов в документообороте
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <returns>Список документов из документооборота</returns>
+        Task<List<Document>> GetDocumentsAsync(Guid docflowId);
+
+        /// <summary>
+        /// Получение документа из документооборота по его идентификатору
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <returns>Документ из документооборота</returns>
+        Task<Document> GetDocumentAsync( Guid docflowId, Guid documentId);
+
+        /// <summary>
+        /// Получение описания документа
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <returns>Описание документа</returns>
+        Task<DocflowDocumentDescription> GetDocumentDescriptionAsync(
+            Guid docflowId,
+            Guid documentId
+        );
+
+        /// <summary>
+        /// Получение списка подписей документа
+        /// </summary>
+        /// <param name="accountId">Идентификатор учетной записи</param>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <returns>Подписи документа</returns>
+        Task<List<Signature>> GetDocumentSignaturesAsync(
+            Guid docflowId,
+            Guid documentId);
+
+        /// <summary>
+        /// Получение подписи документа по идентификатору
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="signatureId">Идентификатор подписи</param>
+        /// <returns>Подпись документа</returns>
+        Task<Signature> GetSignatureAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid signatureId);
+
+        /// <summary>
+        /// Получение контента подписи документа
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="signatureId">Идентификатор подписи</param>
+        /// <returns>Контент подписи документа</returns>
+        Task<byte[]> GetSignatureContentAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid signatureId
+        );
+
+        /// <summary>
+        /// Печать документа
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="contentId">Идентификатор расшифрованного документа в сервисе контентов</param>
+        /// <returns>Результат печати с идентификатором печатной формы документа в сервисе контентов</returns>
+        Task<PrintDocumentResult> PrintDocumentAsync( Guid docflowId, Guid documentId, Guid contentId );
+
+        /// <summary>
+        /// Печать документа
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="contentId">Идентификатор расшифрованного документа в сервисе контентов</param>
+        /// <returns>Задача печати документа</returns>
+        Task<ApiTaskResult<PrintDocumentResult>> StartPrintDocumentAsync( Guid docflowId, Guid documentId, Guid contentId);
+
+        /// <summary>
+        /// Проверка статуса задачи печати по TaskId
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="taskId">Идентификатор задачи</param>
+        /// <returns>Задача печати документа</returns>
+        Task<ApiTaskResult<PrintDocumentResult>> GetPrintTaskAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid taskId
+        );
+
+        /// <summary>
+        /// Дешифрование контента документа Контур.Сертификатом
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="certificate">Сертификат</param>
+        /// <returns>Инициировано дешифрование документа</returns>
+        Task<CloudDecryptionInitResult> StartCloudDecryptDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            byte[] certificate
+        );
+
+        /// <summary>
+        /// Подтверждение дешифрования документа Контур.Сертификатом
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="requestId">Идентификатор запроса, который инициировал дешифрование контента документа</param>
+        /// <param name="code">Код из смс</param>
+        /// <param name="unzip">Флаг распаковки архива. По умолчанию false, контент возвращается сжатым</param>
+        /// <returns></returns>
+        Task<DecryptDocumentResult> ConfirmDocumentDecryptionAsync(
+            Guid docflowId,
+            Guid documentId,
+            string requestId,
+            string code,
+            bool? unzip = null
+        );
+
+        /// <summary>
+        /// Дешифрование контента документа сертификатом DSS
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="certificate">Сертификат</param>
+        /// <param name="unzip">Флаг необходимости распаковки архива. По умолчанию false, контент возвращается сжатым</param>
+        /// <returns>Инициировано дешифрование документа</returns>
+        Task<DssDecryptionInitResult> StartDssDecryptDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            byte[] certificate,
+            bool? unzip = null
+        );
+
+        /// <summary>
+        /// Проверка статуса задачи дешифрования по TaskId
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа</param>
+        /// <param name="taskId">Идентификатор задачи</param>
+        /// <returns>Задача дешифрования документа</returns>
+        Task<ApiTaskResult<DecryptDocumentResult>> GetDssDecryptDocumentTaskAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid taskId
+        );
+
+        /// <summary>
+        /// Распознавание требования
+        /// </summary>
+        /// <param name="docflowId">Идентификатор документооборота</param>
+        /// <param name="documentId">Идентификатор документа требования с типом fns534-demand-attachment</param>
+        /// <param name="contentId"></param>
+        /// <returns>Распознанные данные из файла требования</returns>
+        Task<RecognizeResult> RecognizeDocumentAsync(
+            Guid docflowId,
+            Guid documentId,
+            Guid contentId
+        );
+    }
+}
