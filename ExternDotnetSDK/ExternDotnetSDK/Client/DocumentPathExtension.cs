b0e780577351de6aa5241e86c931d21eed7920cc
ifilipenko <filipenko.in@gmail.com>
2021-07-08 17:47:57 +0200

    getting document

--- ExternDotnetSDK/ExternDotnetSDK/Client/DocumentPathExtension.cs
+++ ExternDotnetSDK/ExternDotnetSDK/Client/DocumentPathExtension.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Kontur.Extern.Client.ApiLevel;
 using Kontur.Extern.Client.ApiLevel.Models.Docflows;
+using Kontur.Extern.Client.ApiLevel.Models.Documents;
 using Kontur.Extern.Client.Model.DocflowFiltering;
 using Kontur.Extern.Client.Paths;
 using Kontur.Extern.Client.Primitives;
@@ -13,6 +14,12 @@ namespace Kontur.Extern.Client
     [PublicAPI]
     public static class DocumentPathExtension
     {
+        public static Task<Document> GetAsync(this in DocumentPath path, TimeSpan? timeout = null)
+        {
+            var apiClient = path.Services.Api;
+            return apiClient.Docflows.GetDocumentAsync(path.AccountId, path.DocflowId, path.DocumentId, timeout);
+        }
+        
         public static IEntityList<DocflowPageItem> RelatedDocflowsList(this in DocumentPath path, DocflowFilterBuilder? filterBuilder = null, TimeSpan? timeout = null)
         {
             return DocflowsList(
