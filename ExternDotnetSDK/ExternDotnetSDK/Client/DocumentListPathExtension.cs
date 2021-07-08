047ce47d740b9e80a20948d0b6890d30388ef3f8
ifilipenko <filipenko.in@gmail.com>
2021-07-08 17:43:58 +0200

    getting documents list

---
+++ ExternDotnetSDK/ExternDotnetSDK/Client/DocumentListPathExtension.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Kontur.Extern.Client.ApiLevel.Models.Documents;
+using Kontur.Extern.Client.Paths;
+
+namespace Kontur.Extern.Client
+{
+    [PublicAPI]
+    public static class DocumentListPathExtension
+    {
+        public static Task<List<Document>> List(this DocumentListPath path, TimeSpan? timeout = null)
+        {
+            var apiClient = path.Services.Api;
+            return apiClient.Docflows.GetDocumentsAsync(path.AccountId, path.DocflowId, timeout);
+        }
+    }
+}
