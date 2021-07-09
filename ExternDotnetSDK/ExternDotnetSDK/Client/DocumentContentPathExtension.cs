1bf428bb4d9b9608ad41b282fe7300cc4b02653a
ifilipenko <filipenko.in@gmail.com>
2021-07-09 13:58:13 +0200

    add long operation to print content of document

---
+++ ExternDotnetSDK/ExternDotnetSDK/Client/DocumentContentPathExtension.cs
@@ -0,0 +1,27 @@
+using System;
+using JetBrains.Annotations;
+using Kontur.Extern.Client.ApiLevel.Models.Common;
+using Kontur.Extern.Client.Paths;
+using Kontur.Extern.Client.Primitives.LongOperations;
+
+namespace Kontur.Extern.Client
+{
+    [PublicAPI]
+    public static class DocumentContentPathExtension
+    {
+        public static ILongOperation<PrintDocumentResult> Print(this in DocumentContentPath path, TimeSpan? timeout = null)
+        {
+            var apiClient = path.Services.Api;
+            var accountId = path.AccountId;
+            var docflowId = path.DocflowId;
+            var documentId = path.DocumentId;
+            var contentId = path.ContentId;
+
+            return new LongOperation<PrintDocumentResult>(
+                () => apiClient.Docflows.StartPrintDocumentAsync(accountId, docflowId, documentId, contentId, timeout),
+                taskId => apiClient.Docflows.GetPrintDocumentTaskAsync(accountId, docflowId, documentId, taskId, timeout),
+                path.Services.LongOperationsPollingStrategy
+            );
+        }
+    }
+}
