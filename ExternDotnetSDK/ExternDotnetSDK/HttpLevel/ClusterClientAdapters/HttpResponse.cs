53d439dc4ceed51709b86271b88c1bd21895cdc0
ifilipenko <filipenko.in@gmail.com>
2021-07-13 10:35:18 +0200

    fix getting bytes/strings from http response

--- ExternDotnetSDK/ExternDotnetSDK/HttpLevel/ClusterClientAdapters/HttpResponse.cs
+++ ExternDotnetSDK/ExternDotnetSDK/HttpLevel/ClusterClientAdapters/HttpResponse.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Text;
 using Kontur.Extern.Client.Exceptions;
 using Kontur.Extern.Client.HttpLevel.Options;
 using Kontur.Extern.Client.HttpLevel.Serialization;
@@ -8,6 +11,7 @@ namespace Kontur.Extern.Client.HttpLevel.ClusterClientAdapters
 {
     internal class HttpResponse : IHttpResponse
     {
+        private static readonly Encoding DefaultEncoding = Encoding.UTF8;
         private readonly Request request;
         private readonly Response response;
         private readonly IRequestBodySerializer serializer;
@@ -19,9 +23,59 @@ namespace Kontur.Extern.Client.HttpLevel.ClusterClientAdapters
             this.serializer = serializer;
         }
 
-        public byte[] GetBytes() => response.Content.ToArray();
-        
-        public string GetString() => response.Content.ToString();
+        public byte[] GetBytes()
+        {
+            if (response.HasContent)
+                return response.Content.ToArray();
+            
+            if (response.HasStream)
+                return ToArray(response.Stream);
+
+            throw Errors.ResponseHasToHaveBody(request.ToString(true, false));
+
+            static byte[] ToArray(Stream stream)
+            {
+                if (stream is MemoryStream memoryStream)
+                    return memoryStream.ToArray();
+                
+                var count = stream.Length - stream.Position;
+                if (count == 0)
+                    return Array.Empty<byte>();
+                // todo: apply an uninitialized array creation
+                //byte[] copy = GC.AllocateUninitializedArray<byte>(count);
+                var copy = new byte[count];
+                // todo: read asynchronously (this should be rare case) and use ValueTask to avoid allocation in the other cases
+                stream.Read(copy, 0, copy.Length);
+                return copy;
+            }
+        }
+
+        public string GetString()
+        {
+            if (response.HasContent)
+                return response.Content.ToString(DefaultEncoding);
+
+            return response.HasStream 
+                ? GetStringFromStream(response.Stream) 
+                : throw Errors.ResponseHasToHaveBody(request.ToString(true, false));
+
+            static string GetStringFromStream(Stream stream)
+            {
+                if (stream is MemoryStream memoryStream)
+                {
+                    if (!memoryStream.TryGetBuffer(out var buffer))
+                    {
+                        buffer = new ArraySegment<byte>(memoryStream.ToArray());
+                    }
+
+                    return DefaultEncoding.GetString(buffer.Array!, buffer.Offset, buffer.Count);
+                }
+
+                using var streamReader = new StreamReader(stream);
+                // todo: read asynchronously (this should be rare case) and use ValueTask to avoid allocation in the other cases
+                return streamReader.ReadToEnd();
+            }
+        }
 
         public TResponseMessage GetMessage<TResponseMessage>()
         {
--- ExternDotnetSDK/ExternDotnetSDKTests/HttpLevel/HttpResponse_Tests.cs
+++ ExternDotnetSDK/ExternDotnetSDKTests/HttpLevel/HttpResponse_Tests.cs
@@ -1,8 +1,10 @@
+#nullable enable
 using System;
 using System.IO;
 using System.Text;
 using FluentAssertions;
 using JetBrains.Annotations;
+using Kontur.Extern.Client.Exceptions;
 using Kontur.Extern.Client.HttpLevel.ClusterClientAdapters;
 using Kontur.Extern.Client.HttpLevel.Options;
 using Kontur.Extern.Client.HttpLevel.Serialization;
@@ -15,18 +17,67 @@ namespace Kontur.Extern.Client.Tests.HttpLevel
     internal class HttpResponse_Tests
     {
         [Test]
+        public void GetBytes_should_fail_when_response_has_no_body()
+        {
+            var httpResponse = CreateHttpResponse();
+
+            Action action = () => httpResponse.GetBytes();
+
+            action.Should().Throw<ContractException>();
+        }
+        
+        [Test]
+        public void GetBytes_should_extract_bytes_from_content()
+        {
+            var bytes = new byte[] {1, 2, 3};
+            var httpResponse = CreateHttpResponse(new Content(bytes));
+
+            var actualBytes = httpResponse.GetBytes();
+            
+            actualBytes.Should().BeEquivalentTo(bytes);
+        }
+        
+        [Test]
+        public void GetBytes_should_extract_bytes_from_memory_stream()
+        {
+            var bytes = new byte[] {1, 2, 3};
+            var httpResponse = CreateHttpResponse(stream: new MemoryStream(bytes));
+
+            var actualBytes = httpResponse.GetBytes();
+            
+            actualBytes.Should().BeEquivalentTo(bytes);
+        }
+        
+        [Test]
+        public void GetBytes_should_extract_bytes_from_stream()
+        {
+            var bytes = new byte[] {1, 2, 3};
+            var httpResponse = CreateHttpResponse(stream: new BufferedStream(new MemoryStream(bytes)));
+
+            var actualBytes = httpResponse.GetBytes();
+            
+            actualBytes.Should().BeEquivalentTo(bytes);
+        }
+        
+        [Test]
+        public void GetMessage_should_fail_when_response_has_no_body()
+        {
+            var httpResponse = CreateHttpResponse(
+                headers: new Headers(1).Set(HeaderNames.ContentType, ContentTypes.Json));
+
+            Action action = () => httpResponse.GetMessage<Dto>();
+
+            action.Should().Throw<ContractException>();
+        }
+        
+        [Test]
         public void GetMessage_should_deserialize_response_stream_to_DTO()
         {
             const string json = @"{""data"":""some data""}";
             var expectedDto = new Dto {Data = "some data"};
-            var httpResponse = new HttpResponse(
-                DummyRequest,
-                new Response(
-                    ResponseCode.Ok,
-                    stream: ToStream(json),
-                    headers: new Headers(1).Set(HeaderNames.ContentType, ContentTypes.Json)),
-                new RequestBodySerializer()
-            );
+            var httpResponse = CreateHttpResponse(
+                stream: ToStream(json),
+                headers: new Headers(1).Set(HeaderNames.ContentType, ContentTypes.Json));
 
             var dto = httpResponse.GetMessage<Dto>();
             
@@ -38,21 +89,91 @@ namespace Kontur.Extern.Client.Tests.HttpLevel
         {
             const string json = @"{""data"":""some data""}";
             var expectedDto = new Dto {Data = "some data"};
-            var httpResponse = new HttpResponse(
-                DummyRequest,
-                new Response(
-                    ResponseCode.Ok,
-                    ToContent(json),
-                    new Headers(1).Set(HeaderNames.ContentType, ContentTypes.Json)),
-                new RequestBodySerializer()
-            );
+            var httpResponse = CreateHttpResponse(
+                ToContent(json),
+                headers: new Headers(1).Set(HeaderNames.ContentType, ContentTypes.Json));
 
             var dto = httpResponse.GetMessage<Dto>();
             
             dto.Should().BeEquivalentTo(expectedDto);
         }
+        
+        [Test]
+        public void GetMessage_should_fail_when_content_type_is_not_a_json()
+        {
+            const string json = @"{""data"":""some data""}";
+            var httpResponse = CreateHttpResponse(
+                ToContent(json),
+                headers: new Headers(1).Set(HeaderNames.ContentType, "application/pdf"));
 
-        private static Request DummyRequest => Request.Get(new Uri("/dummy", UriKind.Relative));
+            Action action = () => httpResponse.GetMessage<Dto>();
+
+            action.Should().Throw<ContractException>();
+        }
+        
+        [Test]
+        public void GetMessage_should_fail_when_content_type_is_absent()
+        {
+            const string json = @"{""data"":""some data""}";
+            var httpResponse = CreateHttpResponse(ToContent(json));
+
+            Action action = () => httpResponse.GetMessage<Dto>();
+
+            action.Should().Throw<ContractException>();
+        }
+        
+        [Test]
+        public void GetString_should_fail_when_response_has_no_body()
+        {
+            var httpResponse = CreateHttpResponse(headers: new Headers(1).Set(HeaderNames.ContentType, ContentTypes.Json));
+
+            Action action = () => httpResponse.GetString();
+
+            action.Should().Throw<ContractException>();
+        }
+        
+        [Test]
+        public void GetString_should_deserialize_response_memory_stream_to_DTO()
+        {
+            const string expectedBody = "some data";
+            var httpResponse = CreateHttpResponse(stream: ToStream(expectedBody));
+
+            var body = httpResponse.GetString();
+            
+            body.Should().BeEquivalentTo(expectedBody);
+        }
+        
+        [Test]
+        public void GetString_should_deserialize_response_stream_to_DTO()
+        {
+            const string expectedBody = "some data";
+            var httpResponse = CreateHttpResponse(stream: new BufferedStream(ToStream(expectedBody)));
+
+            var body = httpResponse.GetString();
+            
+            body.Should().BeEquivalentTo(expectedBody);
+        }
+        
+        [Test]
+        public void GetString_should_deserialize_response_content_to_DTO()
+        {
+            const string expectedBody = "some data";
+            var httpResponse = CreateHttpResponse(ToContent(expectedBody));
+
+            var body = httpResponse.GetString();
+            
+            body.Should().BeEquivalentTo(expectedBody);
+        }
+
+        private static HttpResponse CreateHttpResponse(Content? content = null, Stream? stream = null, Headers? headers = null)
+        {
+            var dummyRequest = Request.Get(new Uri("/dummy", UriKind.Relative));
+            return new HttpResponse(
+                dummyRequest,
+                new Response(ResponseCode.Ok, content, headers, stream),
+                new RequestBodySerializer()
+            );
+        }
 
         private static MemoryStream ToStream(string json) => new(Encoding.UTF8.GetBytes(json));
         
@@ -61,7 +182,7 @@ namespace Kontur.Extern.Client.Tests.HttpLevel
         private class Dto
         {
             [UsedImplicitly]
-            public string Data { get; set; }
+            public string? Data { get; set; }
         }
     }
 }
