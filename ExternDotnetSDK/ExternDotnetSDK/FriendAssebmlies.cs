d4312df7cd9ced0dd55f089c551b9f90a30569d2
ifilipenko <filipenko.in@gmail.com>
2021-07-15 17:38:45 +0200

    allow NSubstitute to create proxy for internal types

--- ExternDotnetSDK/ExternDotnetSDK/FriendAssebmlies.cs
+++ ExternDotnetSDK/ExternDotnetSDK/FriendAssebmlies.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("Kontur.Extern.Client.Tests")]
+[assembly: InternalsVisibleTo("Kontur.Extern.Client.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
