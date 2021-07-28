fe09a96162c5a69b2634ae008bcc648d5f9d7393
ifilipenko <filipenko.in@gmail.com>
2021-07-28 13:42:27 +0200

    fix request dumping

--- ExternDotnetSDK/Kontur.Extern.Client.Testing.End2End/ClusterClient/DumpRequestsAndResponsesTransport.cs
+++ ExternDotnetSDK/Kontur.Extern.Client.Testing.End2End/ClusterClient/DumpRequestsAndResponsesTransport.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,9 +22,10 @@ namespace Kontur.Extern.Client.Testing.End2End.ClusterClient
 
         public async Task<Response> SendAsync(Request request, TimeSpan? connectionTimeout, TimeSpan timeout, CancellationToken cancellationToken)
         {
-            log.Debug($"Request: {DumpRequest(request)}");
+            var (requestText, recreatedRequest) = DumpRequest(request);
+            log.Debug($"Request: {requestText}");
 
-            var response = await transport.SendAsync(request, connectionTimeout, timeout, cancellationToken);
+            var response = await transport.SendAsync(recreatedRequest, connectionTimeout, timeout, cancellationToken);
 
             log.Debug($"Response: {DumpResponse(response)}");
 
@@ -41,16 +43,41 @@ namespace Kontur.Extern.Client.Testing.End2End.ClusterClient
                 return responseDump.ToString();
             }
 
-            static string DumpRequest(Request request)
+            static (string requestText, Request recreatedRequest) DumpRequest(Request request)
             {
+                var recreatedRequest = request;
                 var requestDump = new StringBuilder();
                 requestDump.AppendLine(request.ToString(true, true));
-                if (request.Content != null && request.Headers?.ContentType?.StartsWith("application/octet-stream") != true)
+                if (request.Headers?.ContentType?.StartsWith("application/octet-stream") != true)
                 {
-                    requestDump.AppendLine(request.Content.ToString());
+                    if (request.Content != null)
+                    {
+                        requestDump.AppendLine(request.Content.ToString());
+                    }
+                    else if (request.StreamContent?.Stream is MemoryStream memoryStream)
+                    {
+                        requestDump.AppendLine(DumpStreamAndRewind(memoryStream));
+                        requestDump.AppendLine();
+
+                        recreatedRequest = new Request(request.Method, request.Url, new StreamContent(memoryStream, request.StreamContent.Length), request.Headers);
+                    }
                 }
 
-                return requestDump.ToString();
+                return (requestDump.ToString(), recreatedRequest);
+            }
+        }
+
+        private static string DumpStreamAndRewind(MemoryStream memoryStream)
+        {
+            var position = memoryStream.Position;
+            try
+            {
+                using var streamReader = new StreamReader(memoryStream, leaveOpen: true);
+                return streamReader.ReadToEnd();
+            }
+            finally
+            {
+                memoryStream.Position = position;
             }
         }
 
