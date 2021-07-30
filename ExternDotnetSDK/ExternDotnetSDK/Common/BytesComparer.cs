fd834bcce2d814c9141802d6f2196fc8258982a0
ifilipenko <filipenko.in@gmail.com>
2021-07-30 16:23:25 +0200

    add method to detect zeroing byte arrays

---
+++ ExternDotnetSDK/ExternDotnetSDK/Common/BytesComparer.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace Kontur.Extern.Client.Common
+{
+    public static class BytesComparer
+    {
+        public static bool IsZero(byte[] bytes)
+        {
+            var firstBytes = Math.Min(bytes.Length, 8);
+            for (var i = 0; i < firstBytes; i++)
+            {
+                if (bytes[i] != 0)
+                    return false;
+            }
+            
+            return firstBytes < 8 || IsZeroByFixedLongUnrolled(bytes);
+        }
+
+        /// <summary>
+        /// based on solution from
+        /// https://stackoverflow.com/questions/33294580/sse-instruction-to-check-if-byte-array-is-zeroes-c-sharp
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static unsafe bool IsZeroByFixedLongUnrolled(byte[] data)
+        {
+            fixed (byte* bytes = data)
+            {
+                var len = data.Length;
+                var remained = len%(sizeof (long)*16);
+                var position = (long*) bytes;
+                var alignedEnd = (long*) (bytes + len - remained);
+
+                while (position < alignedEnd)
+                {
+                    if ((*position | *(position + 1) | *(position + 2) | *(position + 3) | *(position + 4) |
+                         *(position + 5) | *(position + 6) | *(position + 7) | *(position + 8) |
+                         *(position + 9) | *(position + 10) | *(position + 11) | *(position + 12) |
+                         *(position + 13) | *(position + 14) | *(position + 15)) != 0)
+                    {
+                        return false;
+                    }
+
+                    position += 16;
+                }
+
+                for (var i = 0; i < remained; i++)
+                {
+                    if (data[len - 1 - i] != 0)
+                        return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ ExternDotnetSDK/ExternDotnetSDKTests/Common/BytesComparer_Tests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Kontur.Extern.Client.Common;
+using NUnit.Framework;
+
+namespace Kontur.Extern.Client.Tests.Common
+{
+    public class BytesComparer_Tests
+    {
+        [TestCaseSource(nameof(ZeroArrays))]
+        [TestCaseSource(nameof(NonZeroArrays))]
+        public void Should_indicate_that_array_contains_only_zeros((string title, byte[] bytes, bool expectedResult) theCase)
+        {
+            var (_, bytes, expectedResult) = theCase;
+            
+            BytesComparer.IsZero(bytes).Should().Be(expectedResult);
+        }
+
+        public static IEnumerable<(string title, byte[] bytes, bool expectedResult)> ZeroArrays
+        {
+            get
+            {
+                return ArraySizes.Select(s => ($"{s} zero bytes", new byte[s], true));
+            }
+        }
+        
+        public static IEnumerable<(string title, byte[] bytes, bool expectedResult)> NonZeroArrays
+        {
+            get
+            {
+                return ArraySizes.Select(s => ($"{s} non-zero bytes", AllocateNonZero(s), false));
+            }
+        }
+
+        private static readonly int[] ArraySizes = {1, 7, 8, 15, 16, 31, 32, 1024, 4096};
+
+        private static byte[] AllocateNonZero(int size)
+        {
+            var bytes = new byte[size];
+            Array.Fill(bytes, byte.MaxValue);
+            return bytes;
+        }
+    }
+}
