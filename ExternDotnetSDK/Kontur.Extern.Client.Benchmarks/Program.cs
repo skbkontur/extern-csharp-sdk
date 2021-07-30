bf460f5e285ce9653718042eb9f84b4863a81d43
ifilipenko <filipenko.in@gmail.com>
2021-07-30 16:27:51 +0200

    benchmark for detecting zeroing byte arrays

---
+++ ExternDotnetSDK/Kontur.Extern.Client.Benchmarks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace Kontur.Extern.Client.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IConfig? config = null;
+            if (Debugger.IsAttached)
+            {
+                config = new DebugInProcessConfig();
+            }
+            
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+        }
+    }
+}
