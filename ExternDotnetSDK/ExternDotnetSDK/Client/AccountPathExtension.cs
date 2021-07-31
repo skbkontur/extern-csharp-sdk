51af9cea3d3ff16f504503d42103d0faa4d62cba
ifilipenko <filipenko.in@gmail.com>
2021-07-31 11:22:05 +0200

    add extension to load certificates of account

--- ExternDotnetSDK/ExternDotnetSDK/Client/AccountPathExtension.cs
+++ ExternDotnetSDK/ExternDotnetSDK/Client/AccountPathExtension.cs
@@ -24,8 +24,35 @@ namespace Kontur.Extern.Client
             var apiClient = path.Services.Api;
             return apiClient.Accounts.TryGetAccountAsync(path.AccountId);
         }
+
+        /// <summary>
+        /// Получение списка сертификатов выбранного аккаунта
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="forAllUsers">Получить сертификаты всех пользователей, которые имеют доступ к указанной учетной записи (только для администратора)</param>
+        /// <returns></returns>
+        public static IEntityList<CertificateDto> Certificates(this in AccountPath path, bool? forAllUsers = null)
+        {
+            var apiClient = path.Services.Api;
+            var accountId = path.AccountId;
+            
+            return new EntityList<CertificateDto>(
+                async (skip, take, timeout) =>
+                {
+                    int intSkip;
+                    int intTake;
+                    checked
+                    {
+                        intSkip = (int) skip;
+                        intTake = (int) take;
+                    }
+
+                    var certificateList = await apiClient.Accounts.GetAccountCertificatesAsync(accountId, intSkip, intTake, forAllUsers, timeout);
+
+                    return (certificateList.Certificates, certificateList.TotalCount);
+                });    
+        }
         
-        public static IEntityList<CertificateDto> Certificates(this in AccountPath path) => throw new NotImplementedException();
         public static IEntityList<Warrant> Warrants(this in AccountPath path) => throw new NotImplementedException();
 
         public static Task DeleteAsync(this in AccountPath path)
