a73745e9b321d5f11d1de9a37412a261ac275586
ifilipenko <filipenko.in@gmail.com>
2021-07-31 11:53:57 +0200

    fix draft creation end2end test

--- ExternDotnetSDK/Kontur.Extern.Client.End2EndTests/Client/DraftPathsExtensions_Tests.cs
+++ ExternDotnetSDK/Kontur.Extern.Client.End2EndTests/Client/DraftPathsExtensions_Tests.cs
@@ -1,21 +1,19 @@
 using System;
+using System.Net;
 using System.Threading.Tasks;
 using FluentAssertions;
 using JetBrains.Annotations;
 using Kontur.Extern.Client.End2EndTests.Client.TestAbstractions;
 using Kontur.Extern.Client.Exceptions;
 using Kontur.Extern.Client.Model.Drafts;
-using Kontur.Extern.Client.Testing.Generators;
+using Kontur.Extern.Client.Model.Numbers;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Kontur.Extern.Client.End2EndTests.Client
 {
-    public class DraftPathsExtensions_Tests : FromAccountEntityPathsTests
+    public class DraftPathsExtensions_Tests : DefaultAccountPathsTests
     {
-        private readonly Randomizer randomizer = new();
-        private readonly AuthoritiesCodesGenerator codesGenerator = new();
-        
         public DraftPathsExtensions_Tests([NotNull] ITestOutputHelper output)
             : base(output)
         {
@@ -24,7 +22,7 @@ namespace Kontur.Extern.Client.End2EndTests.Client
         [Fact]
         public void Get_should_fail_when_try_to_read_non_existent_draft()
         {
-            Func<Task> func = async () => await Context.Drafts.GetDraft(Account.Id, Guid.NewGuid());
+            Func<Task> func = async () => await Context.Drafts.GetDraft(DefaultAccount.Id, Guid.NewGuid());
 
             func.Should().Throw<ApiException>();
         }
@@ -32,33 +30,34 @@ namespace Kontur.Extern.Client.End2EndTests.Client
         [Fact]
         public async Task TryGet_should_return_null_when_try_to_read_non_existent_draft()
         {
-            var draft = await Context.Drafts.GetDraftOrNull(Account.Id, Guid.NewGuid());
+            var draft = await Context.Drafts.GetDraftOrNull(DefaultAccount.Id, Guid.NewGuid());
 
             draft.Should().BeNull();
         }
 
-        [Fact(Skip = "valid recipient codes needed")]
+        [Fact]
         public async Task Should_create_a_new_draft()
         {
-            var newDraft = RandomDraft();
+            var newDraft = NewDraftOfDefaultAccount();
 
-            await using var entityScope = await Context.Drafts.CreateNew(Account.Id, newDraft);
+            await using var entityScope = await Context.Drafts.CreateNew(DefaultAccount.Id, newDraft);
 
             var createdDraft = entityScope.Entity;
-            var loadedDraft = await Context.Drafts.GetDraft(Account.Id, createdDraft.Id);
+            var loadedDraft = await Context.Drafts.GetDraft(DefaultAccount.Id, createdDraft.Id);
             
             loadedDraft.Should().BeEquivalentTo(createdDraft);
         }
 
-        private NewDraft RandomDraft()
+        private NewDraft NewDraftOfDefaultAccount()
         {
-            var payerInn = codesGenerator.PersonInn();
-            var senderInn = codesGenerator.PersonInn();
-            var senderCert = randomizer.Bytes(10);
+            var certInn = LegalEntityInn.Parse(AccountCertificate.Inn);
+            var certKpp = Kpp.Parse(AccountCertificate.Kpp);
+            var senderCert = AccountCertificate.Content;
+
             return new NewDraft(
-                NewDraftPayer.IndividualEntrepreneur(payerInn),
-                NewDraftSender.IndividualEntrepreneur(senderInn, senderCert),
-                DraftRecipient.Ifns("ifns-code")
+                NewDraftPayer.LegalEntityPayer(certInn, certKpp),
+                NewDraftSender.LegalEntity(certInn, certKpp, senderCert).WithIpAddress(IPAddress.Parse("8.8.8.8")),
+                DraftRecipient.Ifns(IfnsCode.Parse("0087"), MriCode.Parse("9660"))
             );
         }
     }
