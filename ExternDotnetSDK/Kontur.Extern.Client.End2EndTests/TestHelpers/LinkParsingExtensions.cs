0987e7f5b9021f6e9615556afb60636c14de330c
ifilipenko <filipenko.in@gmail.com>
2021-08-13 12:56:08 +0200

    add link parsing extension

---
+++ ExternDotnetSDK/Kontur.Extern.Client.End2EndTests/TestHelpers/LinkParsingExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Kontur.Extern.Client.ApiLevel.Models.Common;
+
+namespace Kontur.Extern.Client.End2EndTests.TestHelpers
+{
+    internal static class LinkParsingExtensions
+    {
+        private static readonly Regex GuidRegex = new Regex(
+            @"(\{?[a-zA-Z0-9]{8}(-{0,1})[a-zA-Z0-9]{4}(-{0,1})[a-zA-Z0-9]{4}(-{0,1})[a-zA-Z0-9]{4}(-{0,1})[a-zA-Z0-9]{12}\}?)",
+            RegexOptions.Compiled | RegexOptions.Singleline
+        );
+
+        public static IEnumerable<Guid> ExtractGuids(this Link link)
+        {
+            var matches = GuidRegex.Matches(link.Href.AbsolutePath);
+            foreach (Match match in matches)
+            {
+                yield return Guid.Parse(match.Value);
+            }
+        }
+    }
+}
