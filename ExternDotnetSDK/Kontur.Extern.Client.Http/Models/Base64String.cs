32e5f08282cfdc82efea73810f2320cd5b88e193
ifilipenko <filipenko.in@gmail.com>
2021-08-16 15:15:08 +0200

    check base64 string parameters

--- ExternDotnetSDK/Kontur.Extern.Client.Http/Models/Base64String.cs
+++ ExternDotnetSDK/Kontur.Extern.Client.Http/Models/Base64String.cs
@@ -1,14 +1,28 @@
 using System;
 using System.Text;
+using Kontur.Extern.Client.Http.Exceptions;
 
 namespace Kontur.Extern.Client.Http.Models
 {
     public readonly struct Base64String
     {
-        public static Base64String FromEncoded(string encoded) => new(encoded);
-        public static Base64String Encode(byte[] bytes) => new(Convert.ToBase64String(bytes));
-        public static Base64String Encode(string plainText) => new(Convert.ToBase64String(Encoding.UTF8.GetBytes(plainText)));
-        
+        public static Base64String FromEncoded(string encoded)
+        {
+            if (string.IsNullOrWhiteSpace(encoded))
+                throw Errors.StringShouldNotBeNullOrWhiteSpace(nameof(encoded));
+            return new(encoded);
+        }
+
+        public static Base64String Encode(byte[] bytes) => 
+            new(Convert.ToBase64String(bytes ?? throw new ArgumentNullException(nameof(bytes))));
+        public static Base64String Encode(string plainText)
+        {
+            if (plainText == null)
+                throw new ArgumentNullException(nameof(plainText));
+
+            return new(Convert.ToBase64String(Encoding.UTF8.GetBytes(plainText)));
+        }
+
         private readonly string value;
 
         private Base64String(string encoded) => value = encoded;
