aded23f3d13f1cbe8f73f7d90e3c70412a0c7089
ifilipenko <filipenko.in@gmail.com>
2021-08-24 13:21:06 +0200

    add missed generic docflow description property

--- ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Docflows/DocflowDescription.cs
+++ ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Docflows/DocflowDescription.cs
@@ -1,10 +1,16 @@
-﻿using Kontur.Extern.Client.ApiLevel.Models.JsonConverters;
+﻿using System;
+using Kontur.Extern.Client.ApiLevel.Models.JsonConverters;
 using Newtonsoft.Json;
+// ReSharper disable CommentTypo
 
 namespace Kontur.Extern.Client.ApiLevel.Models.Docflows
 {
     [JsonObject(NamingStrategyType = typeof (KebabCaseNamingStrategy))]
     public abstract class DocflowDescription
     {
+        /// <summary>
+        /// Идентификатор черновика документооборота, если он был создан через API
+        /// </summary>
+        public Guid? OriginalDraftId { get; set; }
     }
 }
