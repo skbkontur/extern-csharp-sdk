649329dbda7782dfb4bfa54c743226b5ed89935d
ifilipenko <filipenko.in@gmail.com>
2021-09-01 10:36:55 +0200

    actualize models of root drafts namespaces

--- ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Drafts/Draft.cs
+++ ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Drafts/Draft.cs
@@ -1,16 +1,43 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
 using Kontur.Extern.Client.ApiLevel.Models.Common;
 using Kontur.Extern.Client.ApiLevel.Models.Drafts.Meta;
 
 namespace Kontur.Extern.Client.ApiLevel.Models.Drafts
 {
+    [PublicAPI]
+    [SuppressMessage("ReSharper", "CommentTypo")]
     public class Draft
     {
+        /// <summary>
+        /// Идентификатор черновика
+        /// </summary>
         public Guid Id { get; set; }
+        
+        /// <summary>
+        /// Ссылки на документообороты
+        /// </summary>
         public Link[] Docflows { get; set; }
+        
+        /// <summary>
+        /// Ссылки на документы черновика
+        /// </summary>
         public Link[] Documents { get; set; }
+        
+        /// <summary>
+        /// Метаинформация черновика
+        /// </summary>
         public DraftMeta Meta { get; set; }
+        
+        /// <summary>
+        /// Статус черновика
+        /// </summary>
         public DraftStatus Status { get; set; }
+        
+        /// <summary>
+        /// Ссылки для работы с черновиком
+        /// </summary>
         public Link[] Links { get; set; }
     }
 }
--- ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Drafts/DraftStatus.cs
+++ ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Drafts/DraftStatus.cs
@@ -1,5 +1,10 @@
-﻿namespace Kontur.Extern.Client.ApiLevel.Models.Drafts
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace Kontur.Extern.Client.ApiLevel.Models.Drafts
 {
+    [PublicAPI]
+    [SuppressMessage("ReSharper", "CommentTypo")]
     public enum DraftStatus
     {
         New,
