2c477a5330461ee6f4c4eac67f3fc70a3465d31c
ifilipenko <filipenko.in@gmail.com>
2021-09-04 11:41:34 +0200

    actualize events

--- ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Events/EventsPage.cs
+++ ExternDotnetSDK/ExternDotnetSDK/ApiLevel/Models/Events/EventsPage.cs
@@ -1,15 +1,41 @@
-﻿using Kontur.Extern.Client.ApiLevel.Models.Common;
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using Kontur.Extern.Client.ApiLevel.Models.Common;
 
 namespace Kontur.Extern.Client.ApiLevel.Models.Events
 {
+    [PublicAPI]
+    [SuppressMessage("ReSharper", "CommentTypo")]
     public class EventsPage
     {
+        /// <summary>
+        /// Идентификатор первого события на странице ленты событий
+        /// </summary>
         public string FirstEventId { get; set; }
-
+        
+        /// <summary>
+        /// Идентификатор следующего события в ленте, с которого нужно продолжать чтение. Передать в параметре fromId
+        /// </summary>
         public string NextEventId { get; set; }
+        
+        /// <summary>
+        /// Число запрошенных событий в параметре take
+        /// </summary>
         public int RequestedCount { get; set; }
+        
+        /// <summary>
+        /// Количество событий, которые вернулись в запросе
+        /// </summary>
         public int ReturnedCount { get; set; }
+        
+        /// <summary>
+        /// События
+        /// </summary>
         public ApiEvent[] ApiEvents { get; set; }
+        
+        /// <summary>
+        /// Ссылки для работы со страницей ленты событий
+        /// </summary>
         public Link[] Links { get; set; }
     }
 }
