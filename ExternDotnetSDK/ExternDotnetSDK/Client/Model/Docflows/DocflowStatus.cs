a2f95d43e59d1e616270c70784fccd7d70b838a7
ifilipenko <filipenko.in@gmail.com>
2021-09-08 16:13:08 +0200

    add DocflowStatus

---
+++ ExternDotnetSDK/ExternDotnetSDK/Client/Model/Docflows/DocflowStatus.cs
@@ -0,0 +1,53 @@
+#nullable enable
+using System;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using Kontur.Extern.Client.Exceptions;
+using Kontur.Extern.Client.Models.Common;
+
+namespace Kontur.Extern.Client.Model.Docflows
+{
+    /// <summary>
+    /// Статус документооборота. Подробнее о значении статуса читайте в [документации](https://docs-ke.readthedocs.io/ru/latest/specification/%D1%81%D1%82%D0%B0%D1%82%D1%83%D1%81%D1%8B%20%D0%94%D0%9E.html)
+    /// </summary>
+    [PublicAPI]
+    [SuppressMessage("ReSharper", "CommentTypo")]
+    public partial struct DocflowStatus
+    {
+        /// <summary>
+        /// Пространство имен статусов документооборота
+        /// </summary>
+        public static readonly Urn Namespace = Urn.Parse("urn:docflow-common-status");
+        
+        private readonly Urn? urn;
+
+        public DocflowStatus(string urn)
+            : this(Urn.Parse(urn))
+        {
+        }
+
+        public DocflowStatus(Urn urn)
+        {
+            this.urn = urn ?? throw new ArgumentNullException(nameof(urn));
+            if (!Namespace.IsParentOf(urn))
+                throw Errors.UrnDoesNotBelongToNamespace(nameof(urn), urn, Namespace);
+        }
+
+        [Pure]
+        public Urn? ToUrn() => urn;
+
+        public override string ToString() => urn?.ToString() ?? string.Empty;
+
+        public bool Equals(DocflowStatus other) => Equals(urn, other.urn);
+
+        public override bool Equals(object? obj) => obj is DocflowStatus other && Equals(other);
+
+        public override int GetHashCode() => urn != null ? urn.GetHashCode() : 0;
+
+        public static bool operator==(DocflowStatus left, DocflowStatus right) => left.Equals(right);
+
+        public static bool operator!=(DocflowStatus left, DocflowStatus right) => !left.Equals(right);
+
+        public static implicit operator DocflowStatus(string value) => new(value);
+    }
+}
---
+++ ExternDotnetSDK/ExternDotnetSDKTests/Client/Model/Docflows/DocflowStatus_Tests.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using FluentAssertions;
+using Kontur.Extern.Client.Model.Docflows;
+using Kontur.Extern.Client.Models.Common;
+using Kontur.Extern.Client.Tests.Client.Model.TestAssertions;
+using Kontur.Extern.Client.Tests.TestHelpers;
+using NUnit.Framework;
+
+namespace Kontur.Extern.Client.Tests.Client.Model.Docflows
+{
+    [TestFixture]
+    internal class DocflowStatus_Tests
+    {
+        [TestFixture]
+        internal class StaticCtor
+        {
+            [Test]
+            public void Should_initialize_namespace_field()
+            {
+                var ns = DocflowStatus.Namespace;
+
+                ns.Should().NotBeNull();
+            }
+        }
+        
+        [TestFixture]
+        internal class Ctor
+        {
+            [TestCase(null)]
+            [TestCase("")]
+            [TestCase(" ")]
+            [TestCase("not a urn")]
+            public void Should_fail_when_given_invalid_urn(string urn)
+            {
+                Action action = () => _ = new DocflowStatus(urn);
+
+                action.Should().Throw<Exception>();
+            }
+
+            [Test]
+            public void Should_fail_when_given_null_urn()
+            {
+                Action action = () => _ = new DocflowStatus((null as Urn)!);
+
+                action.Should().Throw<ArgumentException>();
+            }
+
+            [Test]
+            public void Should_fail_when_urn_not_belong_to_docflow_type_namespace()
+            {
+                Action action = () => _ = new DocflowStatus("urn:document:fss-sedo-error-exchange-error");
+
+                action.Should().Throw<ArgumentException>();
+            }
+
+            [Test]
+            public void Should_initialize_with_given_urn()
+            {
+                var docflowStatus = new DocflowStatus("urn:docflow-common-status:sent");
+
+                docflowStatus.ToString().Should().Be("urn:docflow-common-status:sent");
+            }
+        }
+        
+        [TestFixture]
+        internal class PredefinedStates
+        {
+            [TestCaseSource(nameof(AllPredefinedDocflowStatuses))]
+            public void Should_have_not_empty_values((FieldInfo field, DocflowStatus? docflowStatus) predefinedType)
+            {
+                var (_, docflowStatus) = predefinedType;
+                
+                docflowStatus.Should().NotBeNull();
+                docflowStatus!.Value.ToUrn().Should().NotBeNull();
+            }
+
+            [Test]
+            public void Should_be_unique() => 
+                AllPredefinedDocflowStatuses.MembersShouldHaveUniqueValues();
+
+            private static IEnumerable<(FieldInfo field, DocflowStatus? status)> AllPredefinedDocflowStatuses => 
+                EnumLikeType.AllEnumMembersOfStruct<DocflowStatus>();
+        }
+    }
+}
