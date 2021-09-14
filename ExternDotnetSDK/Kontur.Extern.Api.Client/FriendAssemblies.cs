f7017c1c1b8c7fa5cd8e471bb5643e0204942830
ifilipenko <filipenko.in@gmail.com>
2021-09-14 22:52:29 +0200

    rename client Tests to UnitTests

--- ExternDotnetSDK/Kontur.Extern.Api.Client/FriendAssemblies.cs
+++ ExternDotnetSDK/Kontur.Extern.Api.Client/FriendAssemblies.cs
@@ -1,6 +1,6 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Kontur.Extern.Api.Client.Tests")]
+[assembly: InternalsVisibleTo("Kontur.Extern.Api.Client.UnitTests")]
 [assembly: InternalsVisibleTo("Kontur.Extern.Api.Client.End2EndTests")]
 [assembly: InternalsVisibleTo("Kontur.Extern.Api.Client.Testing")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
