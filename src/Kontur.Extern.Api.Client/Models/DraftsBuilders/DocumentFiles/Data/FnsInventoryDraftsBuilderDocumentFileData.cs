acd636c52a4cd9057cb6eabdd771cc8f921c998b
ifilipenko <filipenko.in@gmail.com>
2021-09-22 15:15:00 +0200

    make drafts builder file data types immutable

--- src/Kontur.Extern.Api.Client/Models/DraftsBuilders/DocumentFiles/Data/FnsInventoryDraftsBuilderDocumentFileData.cs
+++ src/Kontur.Extern.Api.Client/Models/DraftsBuilders/DocumentFiles/Data/FnsInventoryDraftsBuilderDocumentFileData.cs
@@ -8,9 +8,18 @@ namespace Kontur.Extern.Api.Client.Models.DraftsBuilders.DocumentFiles.Data
     public class FnsInventoryDraftsBuilderDocumentFileData : DraftsBuilderDocumentFileData
     {
         /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scannedFileOrder">
+        /// Порядковый номер файла в многостраничном документе. Если документ одностраничный, то файл будет один и передавать в параметре 1 не обязательно. Пример использования параметра: "3" будет означать, что данный файл — третья страница в документе.
+        /// </param>
+        public FnsInventoryDraftsBuilderDocumentFileData(int scannedFileOrder) => 
+            ScannedFileOrder = scannedFileOrder;
+
+        /// <summary>
         /// Порядковый номер файла в многостраничном документе. Если документ одностраничный, то файл будет один и передавать в параметре 1 не обязательно.
         /// Пример использования параметра: "3" будет означать, что данный файл — третья страница в документе. 
         /// </summary>
-        public int ScannedFileOrder { get; set; }
+        public int ScannedFileOrder { get; }
     }
 }
