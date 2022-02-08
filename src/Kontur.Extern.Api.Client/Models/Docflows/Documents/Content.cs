b6d5f25d577218893ad0b5b056b2c51b905e9adf
Michael Vodyanyy <leequidman@gmail.com>
2022-02-08 17:27:18 +0500

    Use Content instead of obsolete content links

--- src/Kontur.Extern.Api.Client/Models/Docflows/Documents/Content.cs
+++ src/Kontur.Extern.Api.Client/Models/Docflows/Documents/Content.cs
@@ -1,14 +1,20 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
-using Kontur.Extern.Api.Client.Models.Common;
 
 namespace Kontur.Extern.Api.Client.Models.Docflows.Documents
 {
     [PublicAPI]
-    [SuppressMessage("ReSharper", "CommentTypo")]
     public class Content
     {
-        public Link? Decrypted { get; set; }
-        public Link? Encrypted { get; set; }
+        public List<DocflowDocumentContents>? DocflowDocumentContents { get; set; }
+    }
+
+    [PublicAPI]
+    public class DocflowDocumentContents
+    {
+        public Guid ContentId { get; set; }
+        public bool Encrypted { get; set; }
+        public bool Compressed { get; set; }
     }
 }
