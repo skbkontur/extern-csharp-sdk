c1c2e00fd747355464e742b60d60c1ff67d65632
elizShtol <shtol@skbkontur.ru>
2022-06-08 17:11:33 +0500

    add benefit payment initiation docflow

---
+++ src/Kontur.Extern.Api.Client/Models/Docflows/Descriptions/Fss/FssSedoBenefitPaymentInitiationResultDescription.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace Kontur.Extern.Api.Client.Models.Docflows.Descriptions.Fss
+{
+    [PublicAPI]
+    public class FssSedoBenefitPaymentInitiationResultDescription: FssSedoDescription
+    {
+        /// <summary>
+        /// ИНН организации, за которую сдается отчет
+        /// </summary>
+        public string? PayerInn { get; set; }
+
+        /// <summary>
+        /// Версия формы документа
+        /// </summary>
+        public FormVersion? FormVersion { get; set; }
+    }
+}
