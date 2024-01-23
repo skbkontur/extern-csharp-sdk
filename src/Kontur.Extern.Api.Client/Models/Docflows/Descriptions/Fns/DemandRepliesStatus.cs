fb4757a0292fc1aad6568a9401c4be1afd5d83df
Igor Ostanin <ostanin.i@skbkontur.ru>
2024-01-23 14:36:22 +0400

    added DemandRepliesStatus, added date and status fields

---
+++ src/Kontur.Extern.Api.Client/Models/Docflows/Descriptions/Fns/DemandRepliesStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Kontur.Extern.Api.Client.Models.Docflows.Descriptions.Fns;
+
+public enum DemandRepliesStatus
+{
+    Unknown = -1,
+    NotRequired,
+    NotSent,
+    InProgress,
+    Accepted,
+    Declined,
+    ReadyToSend
+}
