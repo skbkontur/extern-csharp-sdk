97ff0d8cb4aaaad74eb0639a584f1e09af47b5e5
klim <ks.savelev@skbkontur.ru>
2024-09-17 22:55:58 +0500

    added request transformation

--- src/Kontur.Extern.Api.Client.Http/Configurations/ExternalUrlHttpClientConfiguration.cs
+++ src/Kontur.Extern.Api.Client.Http/Configurations/ExternalUrlHttpClientConfiguration.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 using Kontur.Extern.Api.Client.Http.Exceptions;
 using Kontur.Extern.Api.Client.Http.Retries;
 using Vostok.Clusterclient.Core;
+using Vostok.Clusterclient.Core.Model;
 using Vostok.Clusterclient.Transport;
 
 namespace Kontur.Extern.Api.Client.Http.Configurations
@@ -16,22 +19,35 @@ namespace Kontur.Extern.Api.Client.Http.Configurations
                 throw new ArgumentNullException(nameof(externalUrl));
             if (!externalUrl.IsAbsoluteUri)
                 throw Errors.UrlShouldBeAbsolute(nameof(externalUrl), externalUrl);
-            
+
             this.externalUrl = externalUrl;
         }
 
         public IIdempotentRequestSpecification IdempotentRequests => HttpMethodBasedIdempotentRequestSpecification.SemanticallyIdempotentMethods;
-        
+
         public IRetryStrategyPolicy RetryStrategy => new ExponentialBackOffRetryStrategyPolicy();
+
+        private readonly List<Func<Request, Request>> requestTransforms = new();
+
+        [UsedImplicitly]
+        public ExternalUrlHttpClientConfiguration WithRequestTransform(Func<Request, Request> requestTransform)
+        {
+            requestTransforms.Add(requestTransform);
+
+            return this;
+        }
 
         public void Apply(IClusterClientConfiguration config)
         {
             if (config == null)
                 throw new ArgumentNullException(nameof(config));
-            
+
             config.Logging.LogReplicaRequests = false;
             config.Logging.LogResultDetails = false;
-            
+
+            foreach (var requestTransform in requestTransforms)
+                config.AddRequestTransform(requestTransform);
+
             config.SetupUniversalTransport();
             config.SetupExternalUrlAsSingleReplicaCluster(externalUrl);
         }
