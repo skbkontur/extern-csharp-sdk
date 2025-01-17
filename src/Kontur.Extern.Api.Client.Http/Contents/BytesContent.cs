6a4668260ad6d16bfd4db94bbfb8609166daa47c
Shundeeva Inna <shundeeva@skbkontur.ru>
2025-01-17 14:32:12 +0400

    Fixed upload content in contents client.

--- src/Kontur.Extern.Api.Client.Http/Contents/BytesContent.cs
+++ src/Kontur.Extern.Api.Client.Http/Contents/BytesContent.cs
@@ -6,11 +6,18 @@ namespace Kontur.Extern.Api.Client.Http.Contents
     internal class BytesContent : IHttpContent
     {
         private readonly byte[] bytes;
+        private readonly int offset;
+        private readonly int length;
 
-        public BytesContent(byte[] bytes) => this.bytes = bytes;
+        public BytesContent(byte[] bytes, int offset, int length)
+        {
+            this.bytes = bytes;
+            this.offset = offset;
+            this.length = length;
+        }
 
         public long? Length => bytes.Length;
 
-        public Request Apply(Request request, IJsonSerializer serializer) => request.WithContent(bytes);
+        public Request Apply(Request request, IJsonSerializer serializer) => request.WithContent(bytes, offset, length);
     }
 }
