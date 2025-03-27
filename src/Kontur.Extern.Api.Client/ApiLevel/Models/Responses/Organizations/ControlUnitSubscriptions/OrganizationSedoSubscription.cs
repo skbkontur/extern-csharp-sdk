e43d5467b6a0260975a7b02d8952ffdf4e5738ec
klim <ks.savelev@skbkontur.ru>
2025-03-27 12:54:52 +0500

    added summary comments

--- src/Kontur.Extern.Api.Client/ApiLevel/Models/Responses/Organizations/ControlUnitSubscriptions/OrganizationSedoSubscription.cs
+++ src/Kontur.Extern.Api.Client/ApiLevel/Models/Responses/Organizations/ControlUnitSubscriptions/OrganizationSedoSubscription.cs
@@ -5,8 +5,20 @@ namespace Kontur.Extern.Api.Client.ApiLevel.Models.Responses.Organizations.Contr
 [PublicAPI]
 public class OrganizationSedoSubscription: OrganizationControlUnitSubscription
 {
+    /// <summary>
+    /// True, если RegistrationNumber это РНС СФР, иначе это РНС ФСС
+    /// </summary>
     public bool IsSfr { get; set; }
+    /// <summary>
+    /// Регистрационный номер ФСС/СФР
+    /// </summary>
     public string RegistrationNumber { get; set; }
+    /// <summary>
+    /// Статус подписки
+    /// </summary>
     public SubscriptionStatus SubscriptionStatus { get; set; }
+    /// <summary>
+    /// Статус верификации, связанной с этой подпиской
+    /// </summary>
     public VerificationStatus VerificationStatus { get; set; }
 }
