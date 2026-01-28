e713c3f28460e5dae506f368caecaf602b780cef
Leontev Daniil <leontev.d@skbkontur.ru>
2026-01-28 14:49:15 +0500

    add new drafts builder methods

---
+++ src/Kontur.Extern.Api.Client/ApiLevel/Models/Responses/DraftBuilders/Builders/DraftsBuilderPrepareErrorDocumentResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace Kontur.Extern.Api.Client.ApiLevel.Models.Responses.DraftBuilders.Builders;
+
+[PublicAPI]
+[SuppressMessage("ReSharper", "CommentTypo")]
+public class DraftsBuilderPrepareErrorDocumentResult
+{
+    /// <summary>
+    /// Идентификатор документа, в котором были выявлены ошибки при сборке
+    /// </summary>
+    public Guid DocumentId { get; set; }
+
+    /// <summary>
+    /// Сообщение об ошибке
+    /// </summary>
+    public string? ErrorMessage { get; set; }
+}
